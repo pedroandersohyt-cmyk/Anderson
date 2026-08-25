45c56bf7d9040e6a9675db90c881cb042a2f94ad
pedroandersohyt-cmyk <pedroandersohyt@gmail.com>
2026-08-25 12:48:02 -0300

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,9 @@
+import "./globals.css"
+export const metadata = { title: "Corte no Pix" }
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-BR">
+      <body>{children}</body>
+    </html>
+  )
+}
