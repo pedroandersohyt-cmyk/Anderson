8c3d954696b030c492667ab07e98f859a89c2a91
pedroandersohyt-cmyk <pedroandersohyt@gmail.com>
2026-08-25 12:13:10 -0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,1 @@
-import type { Config } from "tailwindcss";
-const config: Config = { content: ["./app/**/*.{js,ts,jsx,tsx}"], theme: { extend: {} }, plugins: [] };
-export default config;
+export default {};
